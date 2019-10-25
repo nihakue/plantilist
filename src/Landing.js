319af9380f3debaaf7a9418df387cc9700bbaad9
Gabriel West <walden.gabrielw@gmail.com>
2019-10-25 12:44:37 +0100

    Add bookmarklet

--- src/Landing.js
+++ src/Landing.js
@@ -43,6 +43,9 @@ export function Landing() {
         <p>We do, however, gain access to your public pins and boards when you authorize Plantilist with Pinterest. We do not send this data anywhere, and it is only ever used in the app.</p>
         <p>You can view the Plantilist source code <a href="https://github.com/nihakue/plantilist">here</a></p>
       </details>
+      <div>
+        <a href="javascript:(function(){if (!window.location.host.includes('pinterest')) {return;}const path = window.location.pathname;if (path.split('/').length === 4) {window.location.href = `https://plantilist.club${path}`;}})();">Plantilist It</a> <-- <span>Drag this link into your bookmarks bar and click on it when you're viewing a board in pinterest to load it in Plantilist!</span>
+      </div>
     </div>
   )
 }
